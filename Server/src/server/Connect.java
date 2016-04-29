1ccbf1bc078cfdbff5b2bcb978e07966deaf4086
CoolBeans1990 <lagerlof90@gmail.com>
2016-04-29 12:49:49 +0200

    Changes
    Changes in server  and connect

--- Server/src/server/Connect.java
+++ Server/src/server/Connect.java
@@ -1,14 +1,20 @@
 package server;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
- * Class for connecting to a database and get information
- * Created by Gustav on 2016-04-06.
+ * Class for connecting to a database and get information Created by Gustav on
+ * 2016-04-06.
  */
 public class Connect {
 
@@ -16,6 +22,8 @@ public class Connect {
 	private Statement st;
 	private ResultSet rs;
 	private Room room;
+	private HashMap<String, String> combo = new HashMap<String, String>();
+	private ArrayList<String> distinctFloors = new ArrayList<String>();
 
 	/**
 	 * Connects to the given database
@@ -24,9 +32,15 @@ public class Connect {
 
 		try {//
 			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://10.2.13.227:3306/mapster", "Gustav1993", "password");
-//			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mapster", "Guest", "mapster");
-//			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mapster", "root", "");
+			conn = DriverManager.getConnection(
+					"jdbc:mysql://10.2.13.227:3306/mapster", "Gustav1993",
+					"password");
+			// conn =
+			// DriverManager.getConnection("jdbc:mysql://localhost:3306/mapster",
+			// "Guest", "mapster");
+			// conn =
+			// DriverManager.getConnection("jdbc:mysql://localhost:3306/mapster",
+			// "root", "");
 			st = conn.createStatement();
 
 		} catch (Exception e) {
@@ -45,12 +59,13 @@ public class Connect {
 			rs = st.executeQuery(query);
 			System.out.println("Records from database:");
 			while (rs.next()) {
-				
-				room = new Room(rs.getString(1), rs.getInt(2), 
-						rs.getString(3), rs.getString(4));
-				
-				System.out.println("Floor: " + room.getFloor()+ " Name: " + room.getName() 
-						+ " X/Y: " + room.getCoor() + " Path: " + room.getPath());
+
+				room = new Room(rs.getString(1), rs.getInt(2), rs.getString(3),
+						rs.getString(4));
+
+				System.out.println("Floor: " + room.getFloor() + " Name: "
+						+ room.getName() + " X/Y: " + room.getCoor()
+						+ " Path: " + room.getPath());
 			}
 
 		} catch (Exception e) {
@@ -58,31 +73,122 @@ public class Connect {
 		}
 	}
 
-	/**
-	 * Search for a room in the database with the given string, creates a room object
-	 * from the values in the database and returns the room.
-	 * @param searchFor
-	 * @return room
-     */
-	public Room searchedRoom(String searchFor, String building) {
+	public HashMap<String, String> getHashMap() {
+		return combo;
 
-		String query = "select * FROM " + building + " WHERE name = '" + searchFor + "'";
-		
+	}
+
+	public ArrayList<String> getDistinctFloors() {
+		return distinctFloors;
+	}
+
+	public void whichBuilding(String building) throws SQLException {
+
+		switch (building) {
+
+		case "orkanen":
+			getBuilding("orkanen");
+			break;
+
+		case "niagara":
+			getBuilding("niagara");
+			break;
+
+		case "gaddan":
+			getBuilding("gaddan");
+			break;
+		}
+
+	}
+
+	// Retrieves name and coordinates for all rooms of a building
+	public void getBuilding(String building) throws SQLException {
+		String name, coordinates;
+
+		String query = "SELECT name, coordinates FROM " + building;
+		rs = st.executeQuery(query);
+		combo.clear();
+		while (rs.next()) {
+			name = rs.getString("name");
+			coordinates = rs.getString("coordinates");
+			combo.put(name, coordinates);
+
+//			File file = new File(building);
+//			try {
+//				BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+//				for (String p : combo.keySet()) {
+//					bw.write(p + "," + combo.get(p));
+//					bw.newLine();
+//				}
+//				bw.flush();
+//				bw.close();
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+
+		}
+		requestFloors(building);
+	}
+
+	public void requestFloors(String building) {
+		String query = null;
+
+		switch (building) {
+
+		case "orkanen":
+			query = "SELECT floors FROM downloadable LIMIT 0, 5;";
+			break;
+
+		case "niagara":
+			query = "SELECT floors FROM downloadable LIMIT 5, 6;";
+			break;
+		case "gaddan":
+			query = "SELECT floors FROM downloadable LIMIT 11, 4;";
+			break;
+
+		}
+
 		try {
 			rs = st.executeQuery(query);
-			rs.next();
-			
-			room = new Room(rs.getString(1), rs.getInt(2), 
-					rs.getString(3), rs.getString(4));
+			distinctFloors.clear();
+			while (rs.next()) {
+				distinctFloors.add(rs.getString("path"));
 
+			}
 
-//			System.out.println("#" + room.getId() + " Floor: " + room.getFloor()+ " Name: " + room.getName()
-//				+ " X/Y: " + room.getCoor() + " Path: " + room.getPath());
-			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
+	}
+
+	/**
+	 * Search for a room in the database with the given string, creates a room
+	 * object from the values in the database and returns the room.
+	 * 
+	 * @param searchFor
+	 * @return room
+	 */
+	public Room searchedRoom(String searchFor, String building) {
+
+		String query = "select * FROM " + building + " WHERE name = '"
+				+ searchFor + "'";
+
+		try {
+			rs = st.executeQuery(query);
+			rs.next();
+
+			room = new Room(rs.getString(1), rs.getInt(2), rs.getString(3),
+					rs.getString(4));
+
+			// System.out.println("#" + room.getId() + " Floor: " +
+			// room.getFloor()+ " Name: " + room.getName()
+			// + " X/Y: " + room.getCoor() + " Path: " + room.getPath());
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 		return room;
 	}
 }
--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -3,16 +3,18 @@ package server;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 /**
- * Starts a server that receives a room and returns an image (map)
- * Created by Gustav on 2016-04-05.
+ * Starts a server that receives a room and returns an image (map) Created by
+ * Gustav on 2016-04-05.
  */
 public class Server implements Runnable {
 	private ServerSocket serverSocket;
 	private Thread serverThread;
 	private Connect connect = new Connect();
 	private FileFunctions fh = new FileFunctions();
+	private ArrayList<String> recievedBuildings = new ArrayList<String>();
 
 	public Server(int port) {
 		try {
@@ -32,7 +34,8 @@ public class Server implements Runnable {
 		while (true) {
 			try {
 				Socket socket = serverSocket.accept();
-				System.out.println("Client connected! [" + socket.getInetAddress().getHostName() + "]");
+				System.out.println("Client connected! ["
+						+ socket.getInetAddress().getHostName() + "]");
 				new ClientListener(socket).start();
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -47,38 +50,55 @@ public class Server implements Runnable {
 	private class ClientListener extends Thread {
 		private Socket socket;
 		private ObjectOutputStream outputStream;
-        private DataInputStream inputStream;
+		private DataInputStream inputStream;
 
 		public ClientListener(Socket socket) {
 			this.socket = socket;
 		}
 
 		/**
-		 * Receives room-string from android client, search room via connect class and gets info
-		 * from database, returns the path of the file stored on the server.
+		 * Receives room-string from android client, search room via connect
+		 * class and gets info from database, returns the path of the file
+		 * stored on the server.
 		 */
 		public void run() {
 			try {
-                inputStream = new DataInputStream(socket.getInputStream());
-                String request = inputStream.readUTF();
+				inputStream = new DataInputStream(socket.getInputStream());
+				String request = inputStream.readUTF();
+
+				if (!request.contains(":")) {
+					connect.whichBuilding(request);
+					recievedBuildings = connect.getDistinctFloors();
+
+					for (int i = 0; i < recievedBuildings.size(); i++) {
+						fh.sendFile(outputStream, recievedBuildings.get(i));
+					}
+
+					outputStream.writeObject(connect.getHashMap());
+					outputStream.flush();
+
+				}
 
 				fh.splitRoom(request);
-                System.out.println("Input from client: " + fh.getRoomString() + " " + fh.getBuildingString());
+				System.out.println("Input from client: " + fh.getRoomString()
+						+ " " + fh.getBuildingString());
 
-                Room room = connect.searchedRoom(fh.getRoomString(), fh.getBuildingString());
+				Room room = connect.searchedRoom(fh.getRoomString(),
+						fh.getBuildingString());
 				String strImage = room.getPath();
 
-//				Send file to clientx
+				// Send file to clientx
 				outputStream = new ObjectOutputStream(socket.getOutputStream());
 				fh.sendFile(outputStream, strImage);
-				System.out.println("Picture sent to " + socket.getInetAddress() + "!" +  " [" + strImage + "]");
+				System.out.println("Picture sent to " + socket.getInetAddress()
+						+ "!" + " [" + strImage + "]");
 
-				//Get coordinates
-                fh.splitCoor(room.getCoor());
-                System.out.println("X: " + fh.getX() + " Y: " + fh.getY());
-                outputStream.writeInt(fh.getX());
-                outputStream.writeInt(fh.getY());
-                outputStream.flush();
+				// Get coordinates
+				fh.splitCoor(room.getCoor());
+				System.out.println("X: " + fh.getX() + " Y: " + fh.getY());
+				outputStream.writeInt(fh.getX());
+				outputStream.writeInt(fh.getY());
+				outputStream.flush();
 
 			} catch (Exception e) {
 				e.printStackTrace();
