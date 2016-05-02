e13780fc19588fa57b62cf5d5596da5c9c22e254
CoolBeans1990 <lagerlof90@gmail.com>
2016-05-02 09:25:30 +0200

    Changes and added the class BuildingsOfflineGeneric

--- Mapster_0.7/app/src/main/java/se/mah/mapster/mapster_07/BuildingsOfflineGeneric.java
+++ Mapster_0.7/app/src/main/java/se/mah/mapster/mapster_07/BuildingsOfflineGeneric.java
@@ -1,5 +1,11 @@
 package se.mah.mapster.mapster_07;
 
+import android.graphics.Bitmap;
+import android.os.Environment;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -17,6 +23,7 @@ public class BuildingsOfflineGeneric {
     String ip;
     int port;
     String building;
+    Object[] maps;
 
     public BuildingsOfflineGeneric(String building) throws IOException{
         this.building = building;
@@ -45,9 +52,10 @@ public class BuildingsOfflineGeneric {
                 try{
                     int counter = 0;
                 while(o != null){
-                    o = ois.readObject();
+                array    o = ois.readObject();
+
                     counter++;
-                    
+
                 }}catch(ClassNotFoundException e){
                     e.printStackTrace();
                 }
@@ -58,6 +66,72 @@ public class BuildingsOfflineGeneric {
         }
 
 
+    public void saveHashMap(){
+        try {
+            File  directory = new File(Environment.getExternalStorageDirectory() + File.separator + "Mapster" + File.separator + building);
+            File fileInDir = new File(directory + File.separator + building);
+
+            FileOutputStream fos = new FileOutputStream(fileInDir);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(buildingHashMap);
+            oos.flush();
+
+        }catch(IOException e ){
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void floorSaver(){
+
+    }
+
+    private Bitmap receiveFile(String directory, String fileName) throws Exception {
+
+        File fileInDir = new File(directory + File.separator + fileName);
+
+        // read 4 bytes containing the file size
+        byte[] bSize = new byte[4];
+        int offset = 0;
+
+        while (offset < bSize.length) {
+            int bRead = ois.read(bSize, offset, bSize.length - offset);
+            offset += bRead;
+        }
+
+        // Convert the 4 bytes to an int
+        int fileSize;
+        fileSize = (int) (bSize[0] & 0xff) << 24
+                | (int) (bSize[1] & 0xff) << 16
+                | (int) (bSize[2] & 0xff) << 8
+                | (int) (bSize[3] & 0xff);
+
+        // buffer to read from the socket
+        // 8k buffer is good enough
+        byte[] data = new byte[8 * 1024];
+
+        int bToRead;
+        FileOutputStream fos = new FileOutputStream(fileInDir);
+        BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+        while (fileSize > 0) {
+            // make sure not to read more bytes than filesize
+            if (fileSize > data.length) {
+                bToRead = data.length;
+            } else {
+                bToRead = fileSize;
+            }
+
+            int bytesRead = ois.read(data, 0, bToRead);
+            if (bytesRead > 0) {
+                bos.write(data, 0, bytesRead);
+                fileSize -= bytesRead;
+            }
+        }
+        bos.close();
+
+    }
 
 
 }
